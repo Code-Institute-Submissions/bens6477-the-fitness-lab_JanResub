20d3b9d4a4827cc1936eca0d3519ecef07bdaef6
Ben Smith <bens6477@gmail.com>
2022-11-14 18:41:46 +0000

    Added content to items and item detail pages and added views and linked up urls

--- items/views.py
+++ items/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 
 # Create your views here.
-
 def all_items(request):
     """ A view to show all items, including sorting and search queries """
-
     items = Item.objects.all()
-
     context = {
         'items': items,
     }
 
     return render(request, 'items/items.html', context)
+
+
+def item_detail(request, item_id):
+    """ A view to show individual item details """
+
+    item = get_object_or_404(Item, pk=item_id)
+
+    context = {
+        'item': item,
+    }
+
+    return render(request, 'items/item_detail.html', context)
