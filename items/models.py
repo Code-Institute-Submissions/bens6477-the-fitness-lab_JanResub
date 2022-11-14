c79cde102e3a54f48702cb40b39380557d3292bb
Ben Smith <bens6477@gmail.com>
2022-11-14 15:14:35 +0000

    Added the items app, Item and Category models and added product data through fixtures

---
+++ items/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Category(models.Model):
+    """ Model for categories of items """
+    name = models.CharField(max_length=250)
+    display_name = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_display_name(self):
+        return self.display_name
+
+
+class Item(models.Model):
+    """ Model for the details of items """
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=250)
+    brand = models.CharField(max_length=25, null=True, blank=True)
+    description = models.TextField()
+    sku = models.CharField(max_length=250, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    color = models.CharField(max_length=25, null=True, blank=True)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
