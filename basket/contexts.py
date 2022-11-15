18079d55364279c388544651cf07899d43397317
Ben Smith <bens6477@gmail.com>
2022-11-15 10:01:45 +0000

    Added session to context to store items and quantities

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,11 +1,24 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from items.models import Item
 
 def basket_contents(request):
 
     basket_items = []
     total = 0
     item_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        item = get_object_or_404(Item, pk=item_id)
+        total += quantity * item.price
+        item_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'item': item,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- basket/views.py
+++ basket/views.py
@@ -8,18 +8,17 @@ def view_basket(request):
     return render(request, 'basket/basket.html')
 
 
-def add_to_basket(request, product_id):
-    """ Add a quantity of the specified product to the basket """
+def add_to_basket(request, item_id):
+    """ Add a quantity of the specified item to the basket """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
 
-    if product_id in list(basket.keys()):
-        basket[product_id] += quantity
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
     else:
-        basket[product_id] = quantity
+        basket[item_id] = quantity
 
     request.session['basket'] = basket
-    print(request.session['basket'])
     return redirect(redirect_url)
