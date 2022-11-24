5582f2cb9391a33f8f72f843eae9659a5c35cca4
Ben Smith <bens6477@gmail.com>
2022-11-24 00:58:49 +0000

    Added form, admin page, view and styling to template on profiles app

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('user', 'default_phone_number',
+                       'default_address_line1', 'default_address_line2',
+                       'default_city', 'default_county',
+                       'default_country', 'default_postcode')
+
+    fields = ('user', 'default_phone_number',
+              'default_address_line1', 'default_address_line2',
+              'default_city', 'default_county',
+              'default_country', 'default_postcode')
+
+    list_display = ('user', 'default_phone_number', 'default_country')
+
+    ordering = ('-user',)
+
+
+admin.site.register(UserProfile)
